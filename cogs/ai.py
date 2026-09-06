899152a2a800db29eaf2dfa7cf0422ec8966fa93
Raza's Termux <termux@gmail.com>
2026-09-06 19:10:52 +0530

    Fixed Bugs In ai

--- cogs/ai.py
+++ cogs/ai.py
@@ -1,8 +1,9 @@
 import discord
 from discord.ext import commands
 from openai import OpenAI
+import traceback
 from discord import app_commands
-from discord.ui import TextDisplay, LayoutView, Container, Seperator
+from discord.ui import TextDisplay, LayoutView, Container, Separator
 
 
 class AiManager:
@@ -12,15 +13,21 @@ class AiManager:
             api_key="ollama"  # Required by SDK syntax, but ignored by Ollama
             )
         self.client_modal = 'qwen2.5:7b'
+        self.personality = """
+        You are Cardinal, An Ai Assistant, Created By The Cardinals Team.
+        Your modal is CardinalAI1.0.
+        Your purpose is to help users and management in Cardinals Discord Server and help in programming.
+        You are a good coder and reply after some research and have knowledge of discord.
+        """
             
     def get_response(self, prompt: str):
-        response = client.chat.completions.create(
-            model="qwen2.5:7b",
+        response = self.client.chat.completions.create(
+            model=self.client_modal,
             messages=[
-                {"role": "user", "content": prompt}
+                {"role": "system", "content": self.personality},{"role": "user", "content": prompt}
             ]
         )
-        return response.choices[0].content
+        return response.choices[0].message.content
 
 class MessageBox(LayoutView):
     def __init__(self, msg: str):
@@ -30,7 +37,7 @@ class MessageBox(LayoutView):
         
     def _build_box(self):
         container = Container()
-        sep = Seperator()
+        sep = Separator()
         
         message = TextDisplay(self.msg)
         credit_text = TextDisplay('-# Cardinals AI')
@@ -51,7 +58,7 @@ class CardinalAi(commands.Cog):
     @app_commands.command(name='chat', description='Chat with Cardinal Ai')
     async def chat(self, interaction: discord.Interaction, message: str):
         try:
-            await interaction.defer()
+            await interaction.response.defer()
             
             response = self.ai_manager.get_response(message)
             await interaction.followup.send(view=MessageBox(response))
